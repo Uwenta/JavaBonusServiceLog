b5d641f367145a9b9609eda52b4d753b1593b864
ElenaPonomareva <ponomareva@internet.ru>
2022-06-03 23:50:02 +0500

    reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 100_060;
-    boolean registered = true;
+        long amount = 100_060;
+        boolean registered = true;
 
-    int bonus = (int) service.calculate(amount, registered);
-    System.out.println("Начислено бонусов: " + bonus);
-  }
+        int bonus = (int) service.calculate(amount, registered);
+        System.out.println("Начислено бонусов: " + bonus);
+    }
 }
 
